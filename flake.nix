2a20c416c81378af1cd3185f59a60807b0324394
hnakano863 <notchi863@gmail.com>
2023-08-15 11:48:30 +0900

    create flake

---
+++ flake.nix
@@ -0,0 +1,42 @@
+{
+  description = "My emacs, that is, my golden hammer";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+    emacs-overlay.url = "github:nix-community/emacs-overlay";
+  };
+
+  outputs =
+    { self
+    , nixpkgs
+    , flake-utils
+    , emacs-overlay
+    }:
+
+    flake-utils.lib.eachDefaultSystem (system: {
+
+      packages = let
+        pkgs = import nixpkgs {
+          inherit system;
+          overlays = [ emacs-overlay.overlay ];
+        };
+      in
+      rec {
+
+        manamacs = pkgs.emacs;
+
+        default = manamacs;
+
+      };
+
+      devShells = rec {
+
+        manamacs = self.packages."${system}".manamacs;
+
+        default = self.packages."${system}".default;
+
+      };
+
+    });
+}
