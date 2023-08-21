e4d57cd26f68c9ee038e1e0f4ab8f4f839776322
hnakano863 <notchi863@gmail.com>
2023-08-22 02:10:57 +0900

    patch treemacs

--- manamacs/overrides.nix
+++ manamacs/overrides.nix
@@ -1,2 +1,6 @@
 self: super: {
+  treemacs = self.melpaBuild {
+    inherit (super.treemacs) pname ename commit version src recipe meta;
+    packageRequires = super.treemacs.propagatedBuildInputs ++ [ self.doom-modeline ];
+  };
 }
